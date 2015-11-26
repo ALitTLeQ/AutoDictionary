85dffa74861c928baecd00847690466aa17e8b19
ALitTLeQ <ee803804ee@gmail.com>
2015-11-27 00:01:28 +0800

    upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# -*- coding: utf8 -*-
+__author__ = 'ee830804'
+import requests
+from pyquery import PyQuery as pq
+import re
+
+class YahooDictPaser():
+    def __init__(self):
+        self.url = 'https://tw.dictionary.search.yahoo.com/search?p='
+        self.session  = requests.Session()
+
+    def search(self, engWord):
+        def processExplain(str):
+            str = re.sub('\[(.*)\]', '',str)
+            str = re.sub('([0-9]\. )', '',str)
+            return str
+        def processPartOfSpeech(str):
+            str = re.sub('[^a-z\.]', '',str)
+            return str
+
+        response = self.session.get(self.url + engWord).text
+        d = pq(response)
+
+        self.explain = []
+        self.partOfSpeech = []
+        for item in d(".ov-a").items("h4"):
+            self.explain.append(processExplain(item.text()))
+
+
+        for item in d(".fz-s").items(".mb-10"):
+            self.partOfSpeech.append(processPartOfSpeech(item.text()))
+
+        self.pronunciation = re.findall('KK(.*) ',d("#pronunciation_pos").text())
+
+        if len(self.partOfSpeech) > 0 and len(self.explain) > 0:
+            pronunciation = self.pronunciation[0] if len(self.pronunciation)>0 else ''
+            print pronunciation,'\t', self.partOfSpeech[0],'\t', self.explain[0]
+        else:
+            print '\t'
+
+
+if __name__ == '__main__':
+    ydict = YahooDictPaser()
+    file = open('word.txt', 'r')
+    for word in file:
+        ydict.search(word)
